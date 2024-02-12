e12062f13ab419ff65e3affdadcc2361036582a5
SidonieFoulon <sidonie.foulon@inserm.fr>
2024-02-12 18:14:25 +0100

    ajout de l'exemple BW geyser

--- R/backward.r
+++ R/backward.r
@@ -1,4 +1,4 @@
-backward <- function(modele, forward) { 
+backward <- function(modele, forward) {
   Tr <- modele$trans
   p.Em <- modele$p.emiss
 
@@ -8,23 +8,23 @@ backward <- function(modele, forward) {
   alpha <- forward$alpha
   beta <- forward$beta
   delta <- array(NA_real_, dim = c(m, m, l), dimnames = list(rownames(Tr), rownames(Tr), NULL))
-  
+
   phi <- matrix(NA_real_, nrow = m, ncol = l)
   rownames(phi) <- rownames(Tr)
-  
+
   #initialisation du phi
   #delta_l n'a pas de valeur init
   phi[,l] <- beta[,l]
-  
+
   #calcul du delta et phi simultane
   for(i in l:2){
     phia <- ifelse(alpha[,i] == 0, 0, phi[,i] / alpha[,i])
     delta[,,i] <- Tr * outer(beta[,i-1], phia, "*")
     phi[,i-1] <- rowSums(delta[,,i])
   }
-  
-  delta[,,1] <- rep(0,4)
-  
+
+  delta[,,1] <- rep(0,length(delta[,,1]))
+
   res <- list(delta = delta, phi = phi)
   return(res)
 }
