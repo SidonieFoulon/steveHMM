278c277be5540535b758853111d2ba604f6d31e7
HervePerdry <herve.perdry@gmail.com>
2024-02-19 18:22:46 +0100

    neg_log_likelihood_gradient now relies on AD for the derivatives of the model   (but propagates derivatives itself) neg_log_likelihood does not call forward anymore new function : quasi_newton (wrapper for optim)

---
+++ R/quasi_newton.r
@@ -0,0 +1,22 @@
+quasi_newton <- function(theta, obs, modele.fun, trace = FALSE, ...) {
+  last_theta <- theta
+  last_value <- neg_log_likelihood_gradient(theta, obs, modele.fun)
+
+  ff <- function(theta) {
+    if(trace) cat("theta =", theta, "\n")
+    if(!all(theta == last_theta)) {
+      last_theta <<- theta
+      last_value <<- neg_log_likelihood_gradient(theta, obs, modele.fun)
+    }
+    return(last_value$value)
+  }
+
+  dff <- function(theta) {
+    if(!all(theta == last_theta)) {
+      last_theta <<- theta
+      last_value <<- neg_log_likelihood_gradient(theta, obs, modele.fun)
+    }
+    return(last_value$gradient)
+  }
+  optim(theta, ff, dff, method = "L-BFGS-B", ...)
+}
