ddc02c30528aeb68e247f5d3bdfacda1c982a677
HervePerdry <herve.perdry@gmail.com>
2024-03-29 16:32:15 +0100

    completed "two states" example

--- examples/two_states/modele2s-trans.r
+++ examples/two_states/modele2s-trans.r
@@ -1,31 +1,32 @@
-# theta = (a, b, e)
-# a et b = transition probas, e = proba emission error
+# theta = (a, b, e, alpha)
+# a et b = transition probas, e = proba emission error, (alpha, 1 - alpha) = distribution de départ
 # obs = vecteur de 1 et 2
-modele.2s <- function(theta, obs) {
+modele.2s.tr <- function(theta, obs) {
   a <- theta[1]
   b <- theta[2]
   trans <- matrix( c(1-a, a, b, 1-b), byrow = TRUE, nrow = 2, ncol = 2)
-  colnames(trans) <- rownames(trans) <- c("S1", "S2")
-  # etat stationnaire
-  stat <- c(b, a)/(a + b)
+  colnames(trans) <- rownames(trans) <- c("S1", "S2")  
   # proba d'erreur d'émission
   e <- theta[3]
+  # distribution en i = 1
+  alpha <- theta[4]
+  pi <- c(alpha, 1 - alpha)
   p.emiss <- rbind(S1 = ifelse(obs == 1, 1-e, e), S2 = ifelse(obs == 1, e, 1-e))
-  list(trans = trans, pi = stat, p.emiss = p.emiss, theta = theta)
+  list(trans = trans, pi = pi, p.emiss = p.emiss, theta = theta)
 }
 
-M.step.2s <- function(obs, backward) {
+M.step.2s.tr <- function(obs, backward) {
   D11 <- sum(backward$delta[1,1,])
-  D12 <- sum(backward$delta[1,2,]) + backward$phi[2,1]
-  D21 <- sum(backward$delta[2,1,]) + backward$phi[1,1]
+  D12 <- sum(backward$delta[1,2,]) 
+  D21 <- sum(backward$delta[2,1,])
   D22 <- sum(backward$delta[2,2,])
-  a <- backward$theta[1]
-  b <- backward$theta[2]
-  a <- (D12 - a*(1-a)/(a+b)) / (D11 + D12)
-  b <- (D21 - b*(1-b)/(a+b)) / (D21 + D22)
-
+  a <- D12 / (D11 + D12)
+  b <- D21 / (D21 + D22)
+ 
   e <- (sum(backward$phi[2,which(obs == 1)]) + sum(backward$phi[1,which(obs == 2)])) / ncol(backward$phi)
-  c(a, b, e)
+
+  alpha <- backward$phi[1,1]
+  c(a, b, e, alpha)
 }
 
 X.2s <- rep(rep(1:2, 115), c(3,1,6,1,2,1,3,3,13,1,15,1,3,1,7,
@@ -38,11 +39,11 @@ X.2s <- rep(rep(1:2, 115), c(3,1,6,1,2,1,3,3,13,1,15,1,3,1,7,
 12,6,5,3,1,1,10,1,2,1,7,1,2,2,9,4,1,12,9,2,1,10,1,1,1,22,1,8,
 1,4,1,1,2,1,11,1,8,1,11,1,2,1,1,9,6,12))
 
-par.2s <- c(0.1, 0.1, 0.2)
-
+par.2s.tr <- c(0.1, 0.1, 0.2, 0.5)
 
 if(FALSE) {
 require(steveHMM)
-qnem <- QNEM(par.2s, X.2s, modele.2s, M.step.2s, lower = c(0,0,0), upper = c(1,1,1), verbose = TRUE)
-em <- EM(par.2s, X.2s, modele.2s, M.step.2s)
+qnem <- QNEM(par.2s.tr, X.2s, modele.2s.tr, M.step.2s.tr, lower = c(0,0,0,0), upper = c(1,1,1,1), verbose = TRUE)
+em <- EM(par.2s.tr, X.2s, modele.2s.tr, M.step.2s.tr)
 }
+
