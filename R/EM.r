6eda54e7ab86c89393f85f402783f635eeb37473
SidonieFoulon <sidonie.foulon@inserm.fr>
2024-07-16 18:07:14 +0200

    add documentation EM function

--- R/EM.r
+++ R/EM.r
@@ -1,9 +1,27 @@
-# Fonction EM : spécifique à l'exemple choisi pour l'étape M
-# theta = paramètres de début
-# obs = observations
-# modele.fun = une fonction "modèle"
-# M.step.fun = une fonction M step (input : obs, backward, output : theta)
-# it = iterations
+#' Baum-Welch EM algorithm.
+#'
+#'
+#' @param theta the initial parameters
+#' @param obs the observations data
+#' @param modele.fun a model function
+#' @param M.step.fun a M step function :
+#'        - input : obs, backward
+#'        - output : theta
+#' @param max.iter maximum number of iteration of the algorithm (default is 100)
+#' @param trace.theta whether you want to keep theta estimation for each iteration (default is TRUE)
+#' @param epsilon if criteria = "eps", algorithm converges if the difference with the previous iteration is lower than eps (default is 1e-5)
+#' @param reltol if criteria = "reltol", constant related to the stopping criterion, often depending on machine precision (default is sqrt(.Machine$double.eps))
+#' @param criteria stopping criterion (default is reltol)
+#'
+#'
+#' @seealso \code{\link{forward}}
+#' @seealso \code{\link{backward}}
+#'
+#'
+#'
+#' @export
+
+
 EM <- function(theta, obs, modele.fun, M.step.fun, max.iter = 100, trace.theta = TRUE, epsilon = 1e-5, reltol = sqrt(.Machine$double.eps), criteria = c("reltol", "eps")){
   if(is.infinite(max.iter)) trace.theta <- FALSE
   max.iter <- max(max.iter, 3)
